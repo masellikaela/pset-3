d4e5a0eb18201d59a229aac25de532808a8c51c5
Kaela Maselli <kmaselli@ucvts.org>
2019-10-30 10:46:27 -0400

    school work

---
+++ src/sign.js
@@ -0,0 +1,9 @@
+ [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+ const number = ("\nEnter a Number: ")
+
+ if (Number.isNaN(change)) {
+       console.log("invalid")
+ } else if (number>0)
+ console.log("Positive.")
+} else (number<0)
+console.log("Negative.")

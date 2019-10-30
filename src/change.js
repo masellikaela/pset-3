34ef24ee18c270642db50e5e76999f6a4167d86d
Kaela Maselli <kmaselli@ucvts.org>
2019-10-30 10:32:46 -0400

    exerciseoiro 1

--- src/change.js
+++ src/change.js
@@ -1,33 +1,30 @@
 const readlineSync = require("readline-sync");
+const  MIN = 0.01;
+const  MAX = 0.99;
+const QUARTER = 0.25;
+const DIME = 0.10;
+const NICKEL = 0.05;
+const PENNY = 0.01;
 
-const MIN = 0.01;
-const MAX = 0.99;
+const change = Number(readlineSync.question("\nEnter a dollar amount: "));
 
-const quarter = 0.25
-const dime = 0.10
-const nickle = 0.05
-const pennies = 0.01
-
-(C-0.25)
-Q=Q+1
-REPEAT UNTIL C<0.25
-(C-0.10)
-D=D+1
-	REPEAT UNTIL C<0.10
-(C-0.05)
-N=N+1
-REPEAT UNTIL C<0.05
-(C-0.01)
-P=P+1
-REPEAT UNTIL C<0.01
-
-
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
-
-if (Number.isNaN(amount)) {
+if (Number.isNaN(change)) {
       console.log("invalid")
-} else if (amount < MIN || amount > MAX) {
+} else if (change < MIN || change > MAX) {
       console.log("invalid")
-} else {
-    // write your code for making change here
+} else
+
+{let quarteramount=	Math.floor(change/QUARTER);
+  let remainquarter=change%QUARTER;
+
+	let dimeamount= Math.floor(remainquarter/DIME);
+	let remaindime= remainquarter%DIME;
+
+	let nickelamount= Math.floor(remaindime/NICKEL);
+	let remainnickel= remaindime%NICKEL;
+
+	let pennyamount= Math.ceil(remainnickel/PENNY);
+	let remainpenny= remainnickel%PENNY;
+
+	console.log("\n"+ quarteramount + " quarters, " +  dimeamount +" dimes, "+ nickelamount +" nickels, and " + pennyamount +" pennies.")
 }

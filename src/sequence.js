c427f4746c471ff7eba2c128450f57b64489682b
Kaela Maselli <ksmaselli@gmail.com>
2019-11-04 10:01:16 -0500

    Update sequence.js

--- src/sequence.js
+++ src/sequence.js
@@ -7,11 +7,32 @@ let number = Number(readlineSync.question("\nEnter three numbers: \n\n"));
 number= readlineSync.question();
 number= readlineSync.question();
 
- //if (Number.isNaN(number)) {
-       //console.log("\nInvalid.")
- //} else if (number < MIN || number > MAX){
-    //   console.log("\nInvalid.")
- //}
+let numArr = Array.prototype.slice.call(arguments);
+  let truthArray = [];
+
+  for (var num = 0; num < numArr.length; num++) {
+    while (numArr[num + 1] !== undefined) {
+      if (numArr[num] < numArr[num + 1]) {
+        truthArray.push(true);
+      } else {
+        truthArray.push(false);
+      }
+      num++
+    }
+  }
+
+  if (truthArray.includes(false)) {
+    return false;
+  } else {
+    return true;
+  }
+}//test that stuff above?
+
+ /*if (Number.isNaN(number)) {
+   *console.log("\nInvalid.")
+ *} else if (number < MIN || number > MAX){
+    *   console.log("\nInvalid.")
+ */}
  //else if (){
   //     console.log("\nStrictly increasing.")
 // }

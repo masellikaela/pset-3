80ad53c8f137fa97073746a2cde500d559b40ea2
Kaela Maselli <ksmaselli@gmail.com>
2019-11-05 06:12:52 -0500

    Update grades.js

--- src/grades.js
+++ src/grades.js
@@ -3,24 +3,34 @@ const  MIN = 0;
 const  MAX = 100;
 const grade = Number(readlineSync.question("\nEnter a grade: "));
 
+const highA = 100;
+const lowA = 90;
+const highB = 89;
+const lowB = 80;
+const highC = 79
+const lowC = 70;
+const highD = 69;
+const lowD = 60;
+const highF = 59;
+const lowF = 0;
 
 if (Number.isNaN(grade)) {
       console.log("invalid")
 } else if (grade < MIN || change > MAX) {
       console.log("invalid")
-} else if (grade>=90 && grade<=100){
-        console.log("You recieved an A.")
-  
-} else if (grade>=80 && grade<=89){
-        console.log("You recieved a B.")
-  
-}  else if (grade>=70 && grade<=79){
-        console.log("You recieved a C.")
-  
-}  else if (grade>=60 && grade<=69){
-        console.log("You recieved a D.")
-  
-}  else if (grade>=0 && grade<=59){
-        console.log("You recieved a F.")
-}   
-  /// wait its suppose Use const variables to represent the thresholds between A, B, C, D, and F?
+
+} else if (grade >= lowA && grade <= highA) {
+      console.log("\nYou received an A.\n")
+
+} else if (grade >= lowB && grade <= highB) {
+      console.log("\nYou received a B.\n")
+
+} else if (grade >= lowC && grade <=highC) {
+      console.log("\nYou received a C.\n")
+
+} else if (grade >= lowD && grade <=highD) {
+      console.log("\nYou received a D.\n")
+
+} else if (grade >= lowF && grade <=highF) {
+      console.log("\nYou received an F.\n")
+}

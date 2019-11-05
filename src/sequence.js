42ba5bfbd9a81a5d81ddfb0b5449f1fdaaf1746e
Kaela Maselli <ksmaselli@gmail.com>
2019-11-05 17:45:52 -0500

    structure

--- src/sequence.js
+++ src/sequence.js
@@ -1,7 +1,7 @@
 const readlineSync = require("readline-sync");
 
- const MAX = Number.MAX_SAFE_INTEGER;
- const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+const MIN = Number.MIN_SAFE_INTEGER;
 
 let number1 = Number(readlineSync.question("\n\nEnter three numbers: \n\n"));
 number2= Number(readlineSync.question());
@@ -11,22 +11,16 @@ number3= Number(readlineSync.question());
    console.log("\nInvalid.")
 } else if ((number1 && number2 && number3) < MIN || (number1 && number2 && number3) > MAX){
    console.log("\nInvalid.")
-}
-else if (number1<number2 && number2<number3){
+} else if (number1<number2 && number2<number3){
       console.log("\nStrictly increasing.")
-}
-else if (number1<=number2 && number2<=number3){
+} else if (number1<=number2 && number2<=number3){
       console.log("\nIncreasing.")
-
-}else if (number1==number2 && number2==number3 && number1==number3){
+} else if (number1==number2 && number2==number3 && number1==number3){
     console.log("\nEqual.")
-}
-else if (number1>number2 && number2>number3){
+} else if (number1>number2 && number2>number3){
       console.log("\nStrictly Decreasing.")
-}
-else if (number1>=number2 && number2>=number3){
+} else if (number1>=number2 && number2>=number3){
       console.log("\nDecreasing.")
-
 } else if ((number1 >= number2 && number2 <= number3) || (number1 <= number2 && number2 >= number3)) {
       console.log("\nUnordered.")
 }

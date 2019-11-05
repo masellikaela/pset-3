eb208aa65fb183d4fd7e5000e206aab50975f4ef
Kaela Maselli <ksmaselli@gmail.com>
2019-11-05 17:51:19 -0500

    struvtur

--- src/years.js
+++ src/years.js
@@ -1,19 +1,16 @@
 const readlineSync = require("readline-sync");
 
- const MAX = Number.MAX_SAFE_INTEGER;
- const MIN = 0;
+const MAX = Number.MAX_SAFE_INTEGER;
+const MIN = 0;
 
 const year = Number(readlineSync.question("\n\nEnter a year: "));
 
 if (Number.isNaN(year)) {
    console.log("\nInvalid.")
-
 } else if (year < MIN || year > MAX) {
    console.log("\nInvalid.")
-
 } else if (year % 4 == 0) {
    console.log("\n" + year + " is a leap year.")
-
 } else if (year % 4 != 0) {
    console.log("\n" + year + " is not a leap year.")
 }

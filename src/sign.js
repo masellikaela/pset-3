245525664b4350bc1add416bb7b84042c9e61947
Kaela Maselli <ksmaselli@gmail.com>
2019-11-05 17:48:36 -0500

    structure

--- src/sign.js
+++ src/sign.js
@@ -1,22 +1,18 @@
 const readlineSync = require("readline-sync");
 
- const MAX = Number.MAX_SAFE_INTEGER;
- const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+const MIN = Number.MIN_SAFE_INTEGER;
 
+let number = Number(readlineSync.question("\n\nEnter a number: "));
 
- let number = Number(readlineSync.question("\n\nEnter a number: "));
-
- if (Number.isNaN(number)) {
+if (Number.isNaN(number)) {
        console.log("\nInvalid.")
 } else if (number < MIN || number > MAX) {
        console.log("\nInvalid.")
-}
-else if (number>0){
+} else if (number>0){
        console.log("\nPositive.")
-}
-else if(number<0){
+} else if(number<0){
        console.log("\nNegative.")
-}
-else if (number === 0){
+} else if (number === 0){
        console.log("\nZero.")
 }
